509e3787bd054edd6bb157e916d06cfdd0e921d6
Jonas <jonas.pereira.cmd@gmail.com>
2025-10-20 11:03:14 -0300

    Novas classes criadas

--- backend/src/main/java/br/com/ecologica/cadastro/CadastroTipoMateriais.java
+++ backend/src/main/java/br/com/ecologica/cadastro/CadastroTipoMateriais.java
@@ -1,5 +1,17 @@
 package br.com.ecologica.cadastro;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tipos_materiais")
 public class CadastroTipoMateriais {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeTipo;
+    private String descricao;
+    private boolean ativo;
 }
